654bf0ae32c40a1e2870bc884e65be702ab3cc71
ohad-ehi7 <philogeneohad6@gmail.com>
2023-04-26 10:17:20 -0400

    first commit

---
+++ app/Http/Livewire/WishlistIconComponent.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Livewire;
+
+use Livewire\Component;
+
+class WishlistIconComponent extends Component
+{
+    protected $listeners = ['refreshComponent'=> '$refresh']; 
+
+    public function render()
+    {
+        
+        return view('livewire.wishlist-icon-component');
+    }
+}
